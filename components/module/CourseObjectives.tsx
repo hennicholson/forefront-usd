c640904a1385c9ee8023853fc43d84a395a23dbc
Hen Nicholson <hennicholson@Hens-MacBook-Air-2.local>
2025-11-08 17:25:27 -0800

    Fix all button import case sensitivity issues

--- components/module/CourseObjectives.tsx
+++ components/module/CourseObjectives.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { CheckCircle2 } from 'lucide-react'
-import { Button } from '@/components/ui/button'
+import { Button } from '@/components/ui/Button'
 import { modules } from '@/lib/data/modules'
 
 interface CourseObjectivesProps {
